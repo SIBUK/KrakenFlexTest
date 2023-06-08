87d83cb972cd0eb5056b0fe42b63976a9b08644d
Simon Bailey <sibuk2000@gmail.com>
2023-06-08 22:45:18 +0100

    Improved the tests
    Added Mocking to the request tests

--- tests/test_outages.py
+++ tests/test_outages.py
@@ -1,9 +1,18 @@
 import unittest
+from unittest.mock import patch, MagicMock
 from outages import Outages
 
 class TestOutagesClass(unittest.TestCase):
     # Test that the outages class is fetching JSON data from its endpoint correctly
-    def test_get_outages_json(self):
+    @patch('web_request.requests')
+    def test_get_outages_json_success(self, mock_requests):
+        mock_response = MagicMock(status_code=200)
+        mock_response.json.return_value =  \
+            [{'id': 'mock_id_1', 'begin': 'mock_begin_1', 'end': 'mock_end_1'},
+             {'id': 'mock_id_2', 'begin': 'mock_begin_2', 'end': 'mock_end_2'}]
+
+        mock_requests.get.return_value = mock_response
+
         t = Outages()
         json_object, status_code = t.get_outages_json()
 
@@ -11,5 +20,31 @@ class TestOutagesClass(unittest.TestCase):
         self.assertEqual(status_code, 200)
         # Test that is has returned a list
         self.assertIsInstance(json_object, list)
-        # Test that the list contains data
-        self.assertGreater(len(json_object), 0)
+        # Test that the list contains the correct data
+        self.assertEqual(json_object[0]['id'], 'mock_id_1')
+        self.assertEqual(json_object[0]['begin'], 'mock_begin_1')
+        self.assertEqual(json_object[0]['end'], 'mock_end_1')
+        self.assertEqual(json_object[1]['id'], 'mock_id_2')
+        self.assertEqual(json_object[1]['begin'], 'mock_begin_2')
+        self.assertEqual(json_object[1]['end'], 'mock_end_2')
+
+
+    @patch('web_request.requests')
+    def test_get_outages_json_fail(self, mock_requests):
+        mock_response = MagicMock(status_code=403)
+        mock_response.json.return_value = None
+
+        mock_requests.get.return_value = mock_response
+
+        t = Outages()
+        json_object, status_code = t.get_outages_json()
+
+        # Test that the status code is 403 for success
+        self.assertEqual(status_code, 403)
+        # Test that is has returned a list
+        self.assertEqual(json_object, None)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
--- tests/test_site_outages.py
+++ tests/test_site_outages.py
@@ -1,8 +1,34 @@
 import unittest
+from unittest.mock import patch, MagicMock
+from pyspark.resource import requests
+import requests.exceptions
 from site_outages import SiteOutages
 
 class TestSiteOutagesClass(unittest.TestCase):
-    def test_post_site_outages_with_wrong_data_but_correct_site(self):
+    @patch('web_request.requests')
+    def test_post_site_outages_with_valid_site_and_data(self, mock_requests):
+        mock_response = MagicMock(status_code=200)
+        mock_requests.post.return_value = mock_response
+
+        t = SiteOutages()
+
+        json_payload = [{'id': '00da8493-1780-4945-867e-538d2a77ad7d', 'begin': '2022-01-01T00:00:00.000Z', 'end': '2021-11-14T07:43:52.874Z'},
+                        {'id': '034426d9-431b-42ba-85f9-74fe892d783a', 'begin': '2021-01-01T00:00:00.000Z', 'end': '2022-12-15T13:38:16.679Z'},
+                        {'id': '03ac36ee-7466-4e4f-8343-20a0f7a46014', 'begin': '2022-08-20T16:41:22.884Z', 'end': '2022-08-30T01:09:20.303Z'}]
+
+        status_code = t.post_site_outages('some-invalid-site', json_payload)
+
+        self.assertEqual(status_code, 200)
+
+
+    @patch('web_request.requests')
+    def test_post_site_outages_with_invalid_data_but_correct_site(self, mock_requests):
+        mock_response = MagicMock(status_code=400)
+        mock_response.raise_for_status.side_effect = requests.HTTPError('400 Client Error')
+
+        mock_requests.exceptions = requests.exceptions
+        mock_requests.post.return_value = mock_response
+
         t = SiteOutages()
 
         json_payload = [{'id': '00da8493-1780-4945-867e-538d2a77ad7d', 'begin': '2022-01-01T00:00:00.000Z', 'end': '2021-11-14T07:43:52.874Z'},
@@ -15,8 +41,14 @@ class TestSiteOutagesClass(unittest.TestCase):
         self.assertTrue('400 Client Error' in str(context.exception))
 
 
+    @patch('web_request.requests')
+    def test_post_site_outages_with_invalid_site(self, mock_requests):
+        mock_response = MagicMock(status_code=404)
+        mock_response.raise_for_status.side_effect = requests.HTTPError('404 Client Error')
 
-    def test_post_site_outages_with_invalid_site(self):
+        mock_requests.exceptions = requests.exceptions
+        mock_requests.post.return_value = mock_response
+
         t = SiteOutages()
 
         json_payload = [{'id': '00da8493-1780-4945-867e-538d2a77ad7d', 'begin': '2022-01-01T00:00:00.000Z', 'end': '2021-11-14T07:43:52.874Z'},
@@ -27,4 +59,8 @@ class TestSiteOutagesClass(unittest.TestCase):
             t.post_site_outages('some-invalid-site', json_payload)
 
         self.assertTrue('404 Client Error' in str(context.exception))
+
+
+if __name__ == '__main__':
+    unittest.main()
 
